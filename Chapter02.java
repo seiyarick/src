d2fc1e0ea0e1a82f8cdcdfda5cf3dcb317740f84
seiyarick <seiyahirota2@gmail.com>
2022-05-30 17:11:12 +0900

    lesson3 question

--- Chapter02.java
+++ Chapter02.java
@@ -1,7 +1,9 @@
-
+//クラスブロックの始まり
 public class Chapter02 {
+//	メソッドブロックの始まり（インデントしてわかりやすくする）
 	public static void main(String[] args) {
-		System.out.println("こんにちは");
+		System.out.println("Hello WEBCAMP!");
+		System.out.println("Hello Java");
 	}
 
 }
