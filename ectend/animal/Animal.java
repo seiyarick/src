407cbb113085afbdfac5955723996b630ec2fb55
seiyarick <seiyahirota2@gmail.com>
2022-06-14 15:43:52 +0900

    interface

--- ectend/animal/Animal.java
+++ ectend/animal/Animal.java
@@ -1,13 +1,14 @@
 package ectend.animal;
 
+// abstract修飾子を設定して、抽象クラスとして宣言
 public abstract class Animal {
 	protected String name;
 	
+	
+	// 共通メソッドを記述
 	public void eat(){
 		System.out.println(name + "が食事をしました！");
 	}
-	
-	public abstract void cry() {
-		System.out.println(this.name + "が吠えました。ワン！ワン！");
-	}
+	// サブクラスごとに違う処理は、抽象メソッドにしてサブクラスに実装させる。
+	public abstract void cry();
 }
--- extend/Abstract01.java
+++ extend/Abstract01.java
@@ -4,9 +4,15 @@ import ectend.animal.Dog;
 
 public class Abstract01 {
 	public static void main(String[] args) {
+		// Dogクラスのインスタンスを作成
 		Dog dog = new Dog();
+		// 抽象クラス（Animalクラス）のメソッドを呼び出し
 		dog.eat();
+		//抽象メソッドを実装したメソッドを呼び出し
 		dog.cry();
+		
+		// 抽象クラスはインスタンス生成するとコンパイルエラー
+		// Animal animal = new Animal();
 	}
 
 }
